19f043747780cbfd052cd106c24d5bd6b8a2b2e8
Brian Rice <riceb53@gmail.com>
2024-10-20 14:32:23 -0700

    apples create

--- app/controllers/apples_controller.rb
+++ app/controllers/apples_controller.rb
@@ -3,4 +3,14 @@ class ApplesController < ApplicationController
     @apples = Apple.all
     render :index
   end
+
+  def create
+    @apple = Apple.new(
+      variety: params[:variety],
+      color: params[:color],
+      mass: params[:mass],
+    )
+    @apple.save
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/apples" => "apples#index"
-  
+  post "/apples" => "apples#create"
+
 end
